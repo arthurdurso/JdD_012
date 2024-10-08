b424455c6440dadb524dc6312a2efdf151de8e1c
Durso <dursoarthur@gmail.com>
2024-10-08 18:24:54 -0300

    Finalizando exercicios da aula 12

--- src/interface/classes/csv_class.py
+++ src/interface/classes/csv_class.py
@@ -1,31 +1,25 @@
 import pandas as pd
 
 class CsvProcessor:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path:str):
         self.file_path = file_path
         self.df = None
         self.df_filtrado = None
 
+
     def carregar_csv(self):
         self.df = pd.read_csv(self.file_path)
-        return self.df  # Return the DataFrame after loading
 
-    ## receber um str str[]
-    def filtrar_por(self, colunas, atributos):
+    def filtar_por(self, colunas:list, atributos:list):
         if len(colunas) != len(atributos):
-            raise ValueError("Não tem o mesmo número de colunas e atributos")
+            raise ValueError("Colunas e atributos não tem a mesma quantidade.")
         
         if len(colunas) == 0:
             return self.df
         
-        coluna_atual = colunas[0]
-        atributo_atual = atributos[0]
-
-        df_filtrado = self.df[self.df[coluna_atual] == atributo_atual]
-
-        if len(colunas) == 1:
-            return df_filtrado
-        else:
-            return self.filtrar_por(colunas[1:], atributos[1:])
-
-    
+        df_filtrado = self.df  # Inicia com o DataFrame completo
+        
+        for coluna, atributo in zip(colunas, atributos):
+            df_filtrado = df_filtrado[df_filtrado[coluna] == atributo]
+        
+        return df_filtrado
